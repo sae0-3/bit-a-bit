56a2029a3ccafe25c19220fbd4c663f113544fba
Jhoestar <202202883@est.umss.edu>
2025-04-29 18:25:25 -0400

    feat(frontend): Agregar modal para las opciones

--- frontend/src/pages/CreateQuestion.tsx
+++ frontend/src/pages/CreateQuestion.tsx
@@ -3,19 +3,27 @@ import { LuPlus } from 'react-icons/lu'
 
 import { DescriptionEditor } from '../components/DescriptionEditor'
 import { Options } from '../components/Options'
+import { Modal } from '../components/Modal'
 
 export default function CreateQuestion() {
   const [optiones, setOptions] = useState<string[]>(['optionA', 'optionB', 'optionC', 'optionD', 'optionE'])
+  const [viewModal, setviewModal] = useState(false)
 
   return (
     <div className="flex w-full flex-col justify-center items-center gap-4">
       <DescriptionEditor />
 
-      <button className="bg-primary-dark text-white p-2 rounded hover:cursor-pointer flex items-center gap-1">
+      {viewModal && (
+        <Modal title={"ingrese la opcion:"} setValues={setOptions} onClose={() => setviewModal(false)} />
+      )}
+
+      <button className="bg-primary-dark text-white p-2 rounded hover:cursor-pointer flex items-center gap-1"
+        onClick={() => setviewModal(true)}
+      >
         <span>Agregar opciones</span> <LuPlus />
       </button>
 
       <Options values={optiones} setValues={setOptions} />
-    </div>
+    </div >
   )
 }
