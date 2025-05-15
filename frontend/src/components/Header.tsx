20115cc0212e9e64b0b43869c1cdf01efc1a1a3e
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2025-05-15 12:13:42 -0400

    reactor(frontend): mejorar al componente Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,18 @@
-import { LuMenu } from 'react-icons/lu'
+import { Link } from '@tanstack/react-router'
+import { FaUserCircle } from 'react-icons/fa'
 
 export const Header = () => {
   return (
-    <header className="bg-primary-dark text-white p-4 flex justify-between items-center">
-      <h1 className="font-black text-2xl">Question Editor</h1>
+    <header className="h-[10vh] bg-primary-dark">
+      <div className="h-full w-10/12 mx-auto text-white flex justify-between items-center">
+        <Link to="/" className="font-black text-xl">
+          Question Editor
+        </Link>
 
-      <LuMenu className='md:hidden text-3xl' />
-      <ul className="hidden md:flex space-x-4">
-        <li><a href="/">Home</a></li>
-        <li><a href="/question">Question</a></li>
-      </ul>
+        <Link to="/" disabled >
+          <FaUserCircle size={32} />
+        </Link>
+      </div>
     </header>
   )
 }
