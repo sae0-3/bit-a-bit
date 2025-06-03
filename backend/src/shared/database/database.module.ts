82515ee736e00abb0bc5c25db058a837ac5ad647
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2025-06-02 23:37:55 -0400

    fix(backend): agregar correctamente las entidades

--- backend/src/shared/database/database.module.ts
+++ backend/src/shared/database/database.module.ts
@@ -2,6 +2,12 @@ import { DynamicModule, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { Pattern } from 'src/entities/pattern.entity';
+import { QuestionPattern } from 'src/entities/question-pattern.entity';
+import { Question } from 'src/entities/question.entity';
+import { User } from 'src/entities/user.entity';
+import { ValidSolution } from 'src/entities/valid-solution.entity';
+
 @Module({})
 export class DatabaseModule {
   static forRoot(): DynamicModule {
@@ -15,7 +21,7 @@ export class DatabaseModule {
               'DATABASE_URL',
               'postgresql://postgres:1234@localhost:5432/questeditor',
             ),
-            entities: [__dirname + '/../**/*.entity.ts'],
+            entities: [User, Question, QuestionPattern, ValidSolution, Pattern],
             synchronize: true,
             logging: true,
             ssl:
