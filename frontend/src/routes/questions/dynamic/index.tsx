999fddd9a06104044e80b3f4aea8cde1aa9dd4b1
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2025-06-06 16:12:18 -0400

    fix(frontend): corregir problema con la ruta

--- frontend/src/routes/questions/dynamic/index.tsx
+++ frontend/src/routes/questions/dynamic/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, useNavigate } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 import { FaPlus } from 'react-icons/fa'
 
 import { CardQuestion } from '../../../components/CardQuestion'
@@ -10,7 +10,6 @@ export const Route = createFileRoute('/questions/dynamic/')({
 
 function IndexQuestionComponent() {
   const { data, isError, isLoading } = useGetQuestions()
-  const navigate = useNavigate()
 
   if (isError) {
     return (
@@ -25,14 +24,14 @@ function IndexQuestionComponent() {
       <div className="w-10/12 flex flex-col items-center flex-wrap gap-2 lg:flex-row lg:justify-between">
         <h1 className="text-2xl font-bold">Preguntas Creadas</h1>
 
-        <button
-          onClick={() => navigate({ from: "/questions/dynamic/create" })}
-          className="bg-primary-dark w-full max-w-sm py-2 px-4 rounded-lg flex justify-center items-center gap-1 flex-wrap text-white disabled:opacity-50"
+        <Link
+          to="/questions/dynamic/create"
+          className={`bg-primary-dark w-full max-w-sm py-2 px-4 rounded-lg flex justify-center items-center gap-1 flex-wrap text-white ${isLoading ? 'opacity-50' : ''}`}
           disabled={isLoading}
         >
           <span className="text-center">Crear Pregunta</span>
           <FaPlus />
-        </button>
+        </Link>
       </div>
 
       {!!data && data.length === 0 ? (
