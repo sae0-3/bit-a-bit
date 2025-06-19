cbbd22cdce92e2addafec05c967188f20b49e8fc
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2025-06-19 14:33:49 -0400

    fix(frontend): limpiar la secuencia inicial cuando el input este vacio

--- frontend/src/components/AddNumberGame.tsx
+++ frontend/src/components/AddNumberGame.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react"
-import { useQuestionFormStore } from "../stores/question-form.store"
-import { NumberCards } from "./NumberCards"
+import { useState } from 'react'
+
+import { useQuestionFormStore } from '../stores/question-form.store'
+import { NumberCards } from './NumberCards'
 
 export const AddNumberGame = () => {
   const { initialNumber, setInitialNumber } = useQuestionFormStore()
-
   const [number, setNumber] = useState(initialNumber.join(''))
   const [error, setError] = useState('')
 
@@ -18,6 +18,7 @@ export const AddNumberGame = () => {
 
     if (value === '') {
       setError('El número debe ser mayor o igual a 0.')
+      setInitialNumber([])
       return
     }
 
@@ -31,27 +32,28 @@ export const AddNumberGame = () => {
   }
 
   return (
-    <div>
-
+    <>
       <div className="w-full flex flex-col gap-1">
         <label
           htmlFor="numberGame"
           className="font-semibold text-lg"
         >
-          Ingrese el numero para el juego:
+          Ingrese el numero para el juego: <span className="font-extrabold text-red-500">*</span>
         </label>
         <input
-          type="text"
+          type="number"
           placeholder="Ingrese el número"
           name="numberGame"
           id="numberGame"
           className="w-full border-b-2 border-b-gray-300 focus:outline-none focus:border-b-black pb-1"
           value={number}
           onChange={(e) => handleNumberInput(e.target.value)}
+          required
         />
         {error && <p className="text-red-500 text-sm">{error}</p>}
       </div>
+
       <NumberCards number={initialNumber} />
-    </div>
+    </>
   )
 }
