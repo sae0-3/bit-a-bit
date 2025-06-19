216efb9eccead932ba5f344304e897cc0a2ead92
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2025-06-19 18:35:24 -0400

    feat(frontend): agregar formulario para la creación de la question

--- frontend/src/components/AddNumberGame.tsx
+++ frontend/src/components/AddNumberGame.tsx
@@ -6,23 +6,13 @@ import { NumberCards } from './NumberCards'
 export const AddNumberGame = () => {
   const { initialNumber, setInitialNumber } = useQuestionFormStore()
   const [number, setNumber] = useState(initialNumber.join(''))
-  const [error, setError] = useState('')
 
   const handleNumberInput = (value: string) => {
-    setNumber(value)
-
     if (!/^\d*$/.test(value)) {
-      setError('Por favor, ingrese un número válido.')
       return
     }
 
-    if (value === '') {
-      setError('El número debe ser mayor o igual a 0.')
-      setInitialNumber([])
-      return
-    }
-
-    setError('')
+    setNumber(value)
     setInitialNumber(
       value
         .split('')
@@ -38,19 +28,19 @@ export const AddNumberGame = () => {
           htmlFor="numberGame"
           className="font-semibold text-lg"
         >
-          Ingrese el numero para el juego: <span className="font-extrabold text-red-500">*</span>
+          Ingrese la secuencia inicial: <span className="font-extrabold text-red-500">*</span>
         </label>
         <input
-          type="number"
-          placeholder="Ingrese el número"
+          type="text"
+          placeholder="Secuencia inicial (123...)"
           name="numberGame"
           id="numberGame"
           className="w-full border-b-2 border-b-gray-300 focus:outline-none focus:border-b-black pb-1"
           value={number}
           onChange={(e) => handleNumberInput(e.target.value)}
           required
+          inputMode="numeric"
         />
-        {error && <p className="text-red-500 text-sm">{error}</p>}
       </div>
 
       <NumberCards number={initialNumber} />
--- frontend/src/routes/questions/create/index.tsx
+++ frontend/src/routes/questions/create/index.tsx
@@ -2,7 +2,7 @@ import { createFileRoute, useCanGoBack, useRouter } from '@tanstack/react-router
 
 import { AddNumberGame } from '../../../components/AddNumberGame'
 import { DescriptionEditor } from '../../../components/DescriptionEditor'
-import { DropzoneImagesUpload } from '../../../components/DropzoneImagesUpload'
+// import { DropzoneImagesUpload } from '../../../components/DropzoneImagesUpload'
 import { useCreateQuestion } from '../../../hooks/useQuestions'
 import { useQuestionFormStore } from '../../../stores/question-form.store'
 
@@ -26,22 +26,25 @@ function CreateQuestionComponent() {
     resetForm()
   }
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    create()
+  }
+
   return (
     <section className="flex w-full flex-col justify-center items-center gap-5 py-4">
       <h1 className="font-bold text-2xl">Registra tu pregunta</h1>
 
       <div className="w-10/12 flex flex-col justify-center items-center gap-5 max-w-xl">
-        <div className="w-full flex flex-col gap-4">
+        <form
+          className="w-full flex flex-col gap-4"
+          onSubmit={handleSubmit}
+          id="question-create-form"
+        >
           <DescriptionEditor />
-          <DropzoneImagesUpload />
+          {/* <DropzoneImagesUpload /> */}
           <AddNumberGame />
-        </div>
-
-        {isError && (
-          <p className='text-center text-red-500'>
-            Ocurrió un problema al crear la pregunta
-          </p>
-        )}
+        </form>
 
         <div className="w-full flex justify-between items-center gap-2 flex-wrap">
           <button
@@ -52,12 +55,19 @@ function CreateQuestionComponent() {
           </button>
           <button
             className="bg-primary-dark text-white py-2 px-4 rounded-lg hover:cursor-pointer flex items-center justify-center disabled:opacity-50"
-            onClick={() => create()}
+            type="submit"
+            form="question-create-form"
             disabled={isPending}
           >
             <span>{isPending ? 'Registrando...' : 'Registrar pregunta'}</span>
           </button>
         </div>
+
+        {isError && (
+          <p className="text-center text-red-500">
+            Ocurrió un problema al crear la pregunta
+          </p>
+        )}
       </div>
     </section>
   )
