ec9e520bb2893e35a1169f40834d97d48bd09d0a
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2025-06-21 18:17:25 -0400

    feat(frontend): agregar funcionalidad al botón de cerrar sesión

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,17 @@
-import { Link } from '@tanstack/react-router'
-import { IoIosLogOut } from "react-icons/io"
+import { Link, useNavigate } from '@tanstack/react-router'
+import { IoIosLogOut } from 'react-icons/io'
+
+import { useAuthStore } from '../stores/auth.store'
 
 export const Header = () => {
+  const { logout } = useAuthStore()
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    logout()
+    navigate({ to: '/login' })
+  }
+
   return (
     <header className="h-[10vh] bg-primary-dark">
       <div className="h-full w-10/12 max-w-4xl mx-auto text-white flex justify-between items-center gap-5">
@@ -9,13 +19,13 @@ export const Header = () => {
           QuestEditor
         </Link>
 
-        <Link
-          to="/login"
-          className="flex justify-center items-center gap-1 py-2 px-4 border border-white/20 rounded-md hover:border-transparent hover:bg-primary-light/10 hover:text-white transition"
+        <button
+          onClick={handleLogout}
+          className="flex justify-center items-center gap-1 py-2 px-4 border border-white/20 rounded-md hover:border-transparent hover:bg-primary-light/10 hover:text-white hover:cursor-pointer transition"
         >
           <p className="hidden lg:block">Cerrar Sesión</p>
           <IoIosLogOut className="" size={30} />
-        </Link>
+        </button>
       </div>
     </header>
   )
